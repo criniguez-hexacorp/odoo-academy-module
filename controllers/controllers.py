1f2f1c96953df87947c416dad79000f65c31d7eb
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-01-28 08:52:21 -0400

    to the browser

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Academy(http.Controller):
-#     @http.route('/academy/academy/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Academy(http.Controller):
+    @http.route('/academy/academy/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
 #     @http.route('/academy/academy/objects/', auth='public')
 #     def list(self, **kw):
